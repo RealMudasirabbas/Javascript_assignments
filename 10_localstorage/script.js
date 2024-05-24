39940bd91de2aa6bc8402f3baff4c30564539bb9
Mudasir Abbas <mudasirabbas5638201@gmail.com>
2024-05-24 15:29:51 +0500

    loca storage completed

---
+++ 10_localstorage/script.js
@@ -0,0 +1,13 @@
+// local Storage
+
+localStorage.setItem("name","Shariq")
+localStorage.setItem("age","20")
+localStorage.setItem("gender","male")
+
+let name = localStorage.getItem("name")
+let age = localStorage.getItem("age")
+let gender = localStorage.getItem("gender")
+
+console.log(name);
+console.log(age);
+console.log(gender);
