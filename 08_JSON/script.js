dc88329937289527c45df35e009d425f18e3c327
Mudasir Abbas <mudasirabbas5638201@gmail.com>
2024-05-24 14:42:06 +0500

    updated JSON

--- 08_JSON/script.js
+++ 08_JSON/script.js
@@ -14,32 +14,8 @@ const randomData = [
         },
         isActive: true,
     },
-    {
-        id: 2,
-        name: "Jane Smith",
-        email: "jane.smith@example.com",
-        age: 25,
-        address: {
-            street: "456 Maple Ave",
-            city: "Othertown",
-            state: "NY",
-            zip: "67890",
-        },
-        isActive: false,
-    },
-    {
-        id: 3,
-        name: "Sam Brown",
-        email: "sam.brown@example.com",
-        age: 40,
-        address: {
-            street: "789 Oak Dr",
-            city: "Sometown",
-            state: "TX",
-            zip: "13579",
-        },
-        isActive: true,
-    },
+    
+    
 ];
 
 const stringifyObj = JSON.stringify(randomData);
